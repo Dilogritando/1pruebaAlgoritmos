807e93741a9bcbce618cb1e0496595fe8de7bd86
Mariana Cortés Rueda <m.cortes.rueda@gmail.com>
2020-07-30 17:29:31 -0500

    Diagramas de flujo realizados en Miró

---
+++ algoritmos.js
@@ -0,0 +1,16 @@
+Promedio
+
+Mes correspondiente
+
+Resultado de la operacion correspondiente
+
+Determinar el número mayor de los tres.
+
+Promedio de un estudiante a partir de sus tres notas parciales.
+
+Muestre en pantalla la edad del mayor.
+
+
+Elabora un diagrama de flujo con los pasos para iniciar sesión en una cuenta de Facebook
+Elabora un diagrama de flujo con los pasos para preparar cafe
+Elabora un diagrama de flujo con los pasos para calcular el área de un rectángulo
