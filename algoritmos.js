5bd23b0524365a2dc0f0b01dce4b6926a4eabbc9
Mariana Cortés Rueda <m.cortes.rueda@gmail.com>
2020-08-01 11:48:59 -0500

    All algorithms work in console, JS integration phase 1

--- algoritmos.js
+++ algoritmos.js
@@ -1,28 +1,53 @@
 /* Algoritmos definidos */
 
 /* Promedio */
-/* function getAverage(num1,num2){
+function getAverage(num1,num2){
     let average = (num1+num2)/2;
     console.log(average);
-} */
+}
 
 /* Mes correspondiente */
-/* function no2Month(indicator){
+function no2Month(indicator){
     let indicatorNum = parseInt(indicator);
     let months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
     let placeinArray = indicatorNum-1;
     let month = months[placeinArray];
     console.log(month);
-} */
+}
 
-/* Resultado de la operacion correspondiente !!!!!!!!!!!*/
-/* function resultOperations(numONE, operator, numTWO){
-    let resultMath = numONE operator numTWO;
-    console.log(resultMath)
-} */
+/* Resultado de la operacion correspondiente*/
+function resultOperations(numONE, operatora, numTWO){
+    let a = parseInt(numONE); 
+    let b = parseInt(numTWO);
+    let operator = operatora.toString();
+    switch (operator) {
+        case '+':
+            let resultSum = (a+b);
+            console.log(`El resultado de la suma es ${resultSum}`);
+            break;
+        
+        case '-':
+            let resultSubs = a-b;
+            console.log(`El resultado de la resta es ${resultSubs}`);
+            break;
+        
+        case '/':
+            let resultDiv = a/b;
+            console.log(`El resultado de la división es ${resultDiv}`);
+            break;
+        
+        case '*':
+            let resultMult = a*b
+            console.log(`El resultado de la multiplicación es ${resultMult}`);
+            break;
+        
+        default:
+          console.log(`Intenta poner dos números enteros usando cualquiera de las operaciones.`);
+      }
+}
 
 /* Determinar el número mayor de los tres. */
-/* function biggest(num1a,num2a,num3a){
+function biggest(num1a,num2a,num3a){
     if (num1a>num2a, num1a>num3a){
         resultBiggest=num1a;
     } else if (num2a>num1a, num2a>num3a) {
@@ -31,17 +56,17 @@
         resultBiggest=num3a;
     }
     console.log(resultBiggest)
-} */
+}
 
 /* Promedio de un estudiante a partir de sus tres notas parciales. */
-/* function gradeAverage(grade1,grade2,grade3){
+function gradeAverage(grade1,grade2,grade3){
     let g1 =  parseFloat(grade1);
     let g2 =  parseFloat(grade2);
     let g3 =  parseFloat(grade3);
     let fullNum = (g1+g2+g3)/3;
     let resultGrades = fullNum.toFixed(2);
     console.log(resultGrades)
-} */
+}
 
 /* Muestre en pantalla la edad del mayor */
 function showEldest(age1, age2){
@@ -54,4 +79,4 @@ function showEldest(age1, age2){
     }
 }
 
-/* export default Algoritmos */
+export default Algoritmos
--- integration.js
+++ integration.js
@@ -1,15 +1,52 @@
 /* Integración con el HTML */
-import Algoritmos from './algoritmos';
+import Algoritmos from './algoritmos' ; 
+
 /* Promedio */
 getAverage(num1,num2) 
 
+let num1 = document.getElementById("tnum1")
+let num2 = document.getElementById("tnum2")
+document.getElementById("averageButton")
+document.getElementById("averageResult")
+
 /* Mes correspondiente */
-no2Month(num)
+no2Month(indicator)
+
+let indicator = document.getElementById("tindicator")
+document.getElementById("monthButton")
+document.getElementById("getMonth")
 
 /* Resultado de la operacion correspondiente */
+resultOperations(numONE, operatora, numTWO)
+
+let numONE = document.getElementById("tnumONE")
+let operatora = document.getElementById("toperator")
+let numTWO = document.getElementById("tnumTWO")
+document.getElementById("opButton")
+document.getElementById("opResult")
 
 /* Determinar el número mayor de los tres. */
+biggest(num1a,num2a,num3a)
+
+let num1a = document.getElementById("tnum1a")
+let num2a = document.getElementById("tnum2a")
+let num3a = document.getElementById("tnum3a")
+document.getElementById("biggestButton")
+document.getElementById("biggestResult")
 
 /* Promedio de un estudiante a partir de sus tres notas parciales. */
+gradeAverage(grade1,grade2,grade3)
+
+let grade1 = document.getElementById("tg1")
+let grade2 = document.getElementById("tg2")
+let grade3 = document.getElementById("tg3")
+document.getElementById("gradeButton")
+document.getElementById("gradesResult")
 
 /* Muestre en pantalla la edad del mayor */
+showEldest(age1, age2)
+
+let age1 = document.getElementById("tage1")
+let age2 = document.getElementById("tage2")
+document.getElementById("eldestButton")
+document.getElementById("eldestResult")
